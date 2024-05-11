d658916a8f18d901f68bb37aa370bd5a9338c088
Faraz Shokat Ali <156314454+Faraz-Mayo@users.noreply.github.com>
2024-05-11 19:43:25 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Intro from "./components/intro";
+import Nav from "./components/nav";
+
+const App = () => {
+
+
+  return <div className="container"> 
+    <Nav />
+    <Intro />
+  </div>
+};
+
+export default App;
---
+++ src/components/intro.jsx
@@ -0,0 +1,44 @@
+const Intro = () => {
+
+    return <>
+        <main className="intro">
+
+            <div className="intro-content">
+
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+                <div className="intro-btn">
+                    <button>
+                        Shop Now</button>
+                    <button className="secondary-btn">
+                        Cateogry
+                    </button>
+
+
+                </div>
+<div className="shopping">
+    <p>Also Available On</p>
+
+    <div className="shopping-icons">
+
+    <img src="/images/flipkart.png" alt="flipkart-logo" />
+    <img src="/images/amazon.png" alt="amazon-logo" />
+    </div>
+</div>
+
+
+            </div>
+            <div className="intro-img">
+            <img src="/images/shoe_image.png" alt="intro-logo" />
+            </div>
+
+
+
+        </main>
+
+
+    </>
+}
+
+export default Intro;
